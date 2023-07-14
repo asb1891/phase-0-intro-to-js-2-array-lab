598f1bcc57167ae0376193e884ca1379b1a404d1
asb1891 <137960726+asb1891@users.noreply.github.com>
2023-07-14 09:16:04 -0400

    completed array lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop("Ralph");
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift("Milo");
+}
+function appendCat(name) {
+    const coolCats = [...cats, "Broom"];
+    return coolCats;
+}
+function prependCat(name) {
+    const coolCats = ["Arnold", ...cats];
+    return coolCats;
+}
+function removeLastCat(name) {
+    const copyOfCats = cats.slice();
+    copyOfCats.pop();
+    return copyOfCats;
+}
+function removeFirstCat() {
+    const ccopyOfCats = cats.slice();
+    ccopyOfCats.shift();
+    return ccopyOfCats;
+}
